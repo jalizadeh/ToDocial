2e30d1446b91d815c05025e08ff40788289a89f7
j-alizadeh <jalizade3d@yahoo.com>
2020-02-21 20:34:18 +0100

    login model name

--- src/main/java/com/jalizadeh/springboot/web/controller/LoginController.java
+++ src/main/java/com/jalizadeh/springboot/web/controller/LoginController.java
@@ -1,13 +1,18 @@
 package com.jalizadeh.springboot.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
+	//Model is exchanged between Controller and View
+	
 	@RequestMapping("/login")
-	public String LoginMessage() {
+	public String LoginMessage(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
 		return "login";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-My first JSP..
+My first JSP... Welcome ${name}
 </body>
 </html>
