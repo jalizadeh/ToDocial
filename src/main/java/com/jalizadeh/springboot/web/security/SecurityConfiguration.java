81ecf3574aa849361f39b7e3af286405343f1d8f
j-alizadeh <jalizade3d@yahoo.com>
2020-02-24 15:38:56 +0100

    Web Security

---
+++ src/main/java/com/jalizadeh/springboot/web/security/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.jalizadeh.springboot.web.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	public void configureGloablSecurity(AuthenticationManagerBuilder auth) throws Exception{
+		//auth.inMemoryAuthentication().withUser("javad").password("12345").roles("USER", "ADMIN");
+		
+		auth.inMemoryAuthentication()
+        .withUser("javad").password("{noop}12345").roles("USER", "ADMIN")
+        .and()
+        .withUser("admin").password("{noop}admin").roles("ADMIN");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/login").permitAll()
+				.antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
+				.formLogin();
+	}
+}
