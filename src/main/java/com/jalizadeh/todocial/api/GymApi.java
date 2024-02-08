0323bdb4ed7ee6f9fb77a9bb11cd230f8d50bfd2
jalizadeh <jalizade3d@yahoo.com>
2024-02-08 20:34:47 +0100

    [Test] Gym API tests (paused)

--- src/main/java/com/jalizadeh/todocial/api/GymApi.java
+++ src/main/java/com/jalizadeh/todocial/api/GymApi.java
@@ -2,7 +2,7 @@ package com.jalizadeh.todocial.api;
 
 import com.jalizadeh.todocial.model.gym.*;
 import com.jalizadeh.todocial.model.gym.dto.GymWorkoutLogSetRep_DTO;
-import com.jalizadeh.todocial.repository.gym.*;
+import com.jalizadeh.todocial.service.GymService;
 import com.jalizadeh.todocial.utils.GymUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,33 +14,21 @@ import java.util.Optional;
 public class GymApi {
 
     @Autowired
-    private GymPlanRepository gymRepository;
-
-    @Autowired
-    private GymPlanWeekDayRepository gymWeekRepository;
-
-    @Autowired
-    private GymDayRepository gymDayRepository;
-
-    @Autowired
-    private GymDayWorkoutRepository gymDayWorkoutRepository;
-
-    @Autowired
-    private GymWorkoutLogRepository gymWorkoutLogRepository;
+    private GymService gymService;
 
     @GetMapping("/api/v1/gym/plan")
     public List<GymPlan> getAllPlans(){
-        return gymRepository.findAll();
+        return gymService.findAll();
     }
 
     @GetMapping("/api/v1/gym/plan/{planId}/week")
     public List<GymPlanWeekDay> getAllWeeksOfPlan(@PathVariable("planId") Long planId){
-        return gymWeekRepository.findAllByPlanId(planId);
+        return gymService.findAllWeeksByPlanId(planId);
     }
 
     @GetMapping("/api/v1/gym/plan/{planId}/day")
     public List<GymDay> getAllDaysOfPlan(@PathVariable("planId") Long planId){
-        return gymDayRepository.findAllByPlanId(planId);
+        return gymService.findAllDaysByPlanId(planId);
     }
 
     @GetMapping("/api/v1/gym/plan/{planId}/workout")
@@ -62,6 +50,7 @@ public class GymApi {
             @PathVariable("planId") Long planId,@PathVariable("week") Long week, @PathVariable("dayId") Long dayId,
              @PathVariable("workoutId") Long workoutId){
 
+        /*
         //return gymDayWorkoutRepository.findAllByPlanIdAndDayId(planId, dayId);
         Optional<GymDayWorkout> workout = gymDayWorkoutRepository.findById(workoutId);
         if(!workout.isPresent()){
@@ -70,13 +59,14 @@ public class GymApi {
 
         GymDayWorkout foundWorkout = workout.get();
         //return gymWorkoutLogRepository.findAllByWeekAndDayWorkout(week, foundWorkout);
+         */
         return null;
     }
 
 
     @PostMapping("/api/v1/gym/workoutLogNoteParser")
-    public List<GymWorkoutLogSetRep_DTO> logParser(@RequestBody String log){
-        return GymUtils.workoutLogNoteParser(log);
+    public List<GymWorkoutLogSetRep_DTO> parseLogNote(@RequestBody String log){
+        return gymService.parseLogNote(log);
     }
 
 }
